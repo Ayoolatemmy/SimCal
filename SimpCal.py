5bb40e7bc278a788b149056ebe73131ab430e436
Ayoolatemmy <temitopeayoola2024@gmail.com>
2024-09-08 16:37:44 +0100

    Add files via upload

---
+++ SimpCal.py
@@ -0,0 +1,57 @@
+#  Addition 
+number1 = input("What is your first number? ")
+number2 = input("What is your second number? ")
+result = int(number1) + int(number2)
+print("The result of the addition is:",result)
+
+# Subtraction
+number1 = input("What is your first number? ")
+number2 = input("What is your second number? ")
+result = int(number1) - int(number2)
+print("The result of the subtraction is:", result)
+
+# Multiplication
+number1 = input("What is your first number? ")
+number2 = input("What is your second number? ")
+result = int(number1) * int(number2)
+print("The result of the multiplication is:", result)
+
+# Division
+number1 = input("What is your first number? ")
+number2 = input("What is your second number? ")
+result = int(number1) / int(number2)
+print("The result of the division is:", result)
+
+# Define a function for addition
+def addition():
+    number1 = input("What is your first number? ")
+    number2 = input("What is your second number? ")
+    result = int(number1) + int(number2)
+    print("The result of the addition is:",result)
+
+# Define a function for subtraction
+def subtraction():
+    number1 = input("What is your first number? ")
+    number2 = input("What is your second number? ")
+    result = int(number1) - int(number2)
+    print("The result of the subtraction is:", result)
+
+# Define a function for multiplication
+def multiplication():
+    number1 = input("What is your first number? ")
+    number2 = input("What is your second number? ")
+    result = int(number1) * int(number2)
+    print("The result of the multiplication is:", result)
+
+# Define a function for division
+def division():
+    number1 = input("What is your first number? ")
+    number2 = input("What is your second number? ")
+    result = int(number1) / int(number2)
+    print("The result of the division is:", result)
+
+# Call the functions
+addition()
+subtraction()
+multiplication()
+division()
