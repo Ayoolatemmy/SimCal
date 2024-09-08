2e77589a73e1734a276461075022a2d219073acb
Ayoolatemmy <temitopeayoola2024@gmail.com>
2024-09-08 20:38:53 +0100

    Update SimpCal.py

--- SimpCal.py
+++ SimpCal.py
@@ -1,27 +1,3 @@
-#  Addition 
-number1 = input("What is your first number? ")
-number2 = input("What is your second number? ")
-result = int(number1) + int(number2)
-print("The result of the addition is:",result)
-
-# Subtraction
-number1 = input("What is your first number? ")
-number2 = input("What is your second number? ")
-result = int(number1) - int(number2)
-print("The result of the subtraction is:", result)
-
-# Multiplication
-number1 = input("What is your first number? ")
-number2 = input("What is your second number? ")
-result = int(number1) * int(number2)
-print("The result of the multiplication is:", result)
-
-# Division
-number1 = input("What is your first number? ")
-number2 = input("What is your second number? ")
-result = int(number1) / int(number2)
-print("The result of the division is:", result)
-
 # Define a function for addition
 def addition():
     number1 = input("What is your first number? ")
